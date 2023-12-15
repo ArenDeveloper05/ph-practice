f692a36599487db14f77a3e086a6114ca8769a80
muradiian <muradyannorayr6@gmail.com>
2023-12-15 23:40:44 +0300

    new sections --update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,16 @@
+
+import MainPageBanner from '../main_page_banner/MainPageBanner'
+import ProductCategories from '../productCategories/ProductCategories'
+
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+      <>
+        <div className="home">
+          <MainPageBanner/>
+          <ProductCategories/>
+        </div>;
+      </>
+  )
 };
 
 export default Home;
