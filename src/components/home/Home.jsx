dcedf89cdef6fd8a0c1592b84112d0b81cf7c250
muradiian <muradyannorayr6@gmail.com>
2023-12-22 23:48:39 +0400

    new-sections

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,7 @@
 
 import MainPageBanner from '../main_page_banner/MainPageBanner'
 import ProductCategories from '../productCategories/ProductCategories'
+import ProductsAbout from '../productsAbout/ProductsAbout';
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
         <div className="home">
           <MainPageBanner/>
           <ProductCategories/>
+          <ProductsAbout/>
         </div>;
       </>
   )
